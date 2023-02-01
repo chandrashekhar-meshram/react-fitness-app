2ab512d2c77b97bd34f2bf188450df2c0466d25c
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-02-01 19:31:43 +0530

    Navbar, Home and ExerciseDeatail created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
-import "./style.css";
+import {Route, Routes} from "react-router-dom";
+import {Box} from "@mui/material";
+
+
+// import "./style.css";
+import "./App.css";
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+
 
 
 export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-
-    </div>
+  return (     
+      <Box width='400px' border="1px solid black">
+        <Navbar />
+      </Box>    
   );
 }
+
+ {/* <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p> */}
