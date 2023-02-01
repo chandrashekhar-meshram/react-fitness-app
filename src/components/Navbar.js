359e3108d7ac558b653a3392d717e984c05a1087
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-02-02 00:30:35 +0530

    navbar.js adited

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,45 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 //import { Link } from 'react-router-dom';
-import {Stack} from "@mui/material";
+import { Stack } from '@mui/material';
 
 import Logo from '../assets/images/Logo.png';
 
 const Navbar = () => {
-
   return (
-    <Stack>
-      {/* <Link>
-        <img src='https://github.com/chandrashekhar-meshram/react-fitness-app/blob/react-projects/src/assets/images/Logo.png' />
-      </Link> */}
-      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPunE1gn8aCL6sUk8vm1sggk2Qd4GCfd5kUhq0Zs8Oj3W3GDHMJ6l7acZu3OsHu2SCFDE&usqp=CAU' alt='navbar'/>
+    <Stack
+      direction='row'
+      justifyContent='space-around'
+      sx={{gap: {sm: '122px', xs: '40px'}, mt: {sm: '32px', xs: '20px'}}}
+      px='20px'
+    >
+      <Link to="/">
+        <img
+          src="https://stackblitz.com/files/react-fitness-app/github/chandrashekhar-meshram/react-fitness-app/react-projects/assets/images/Logo.png"
+          alt="navbar-img"
+          style={{ width: '48px', height: '48px', margin: '0 20px' }}
+        />
+      </Link>
+      <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
+        <Link
+          to="/"
+          style={{
+            textDecoration: 'none',
+            color: '#3A1212',
+            borderBottom: '3px solid #FF2625',
+          }}
+        >
+          Home
+        </Link>
+        <a
+          href="#exercises"
+          style={{ textDecoration: 'none', color: '#3A1212' }}
+        >
+          Exercises
+        </a>
+      </Stack>
     </Stack>
-  )
-}
+  );
+};
 
 export default Navbar;
