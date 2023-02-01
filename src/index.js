5bdc6dc2e7d20bca7852dbb7e00b90630e380b30
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-02-01 16:47:49 +0530

    dependencies added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import ReactDom from 'react-dom';
 
 import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+//const root = ReactDom.createRoot(document.getElementById("root"));
+
 root.render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
